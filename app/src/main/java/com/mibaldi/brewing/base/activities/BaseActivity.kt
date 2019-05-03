4b5df224a6b376fa1953e0efc892042e3129f408
Mikel Balduciel Diaz <mibaldi2@gmail.com>
2019-05-03 16:25:25 +0200

    gitflow-feature-stash: initApp
    Fixes #4

--- app/src/main/java/com/mibaldi/brewing/base/activities/BaseActivity.kt
+++ app/src/main/java/com/mibaldi/brewing/base/activities/BaseActivity.kt
@@ -1,6 +1,6 @@
 package com.mibaldi.brewing.base.activities
 
-import android.support.v7.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatActivity
 
 abstract class BaseActivity : AppCompatActivity() {
 
--- app/src/main/java/com/mibaldi/brewing/ui/activities/MainActivity.kt
+++ app/src/main/java/com/mibaldi/brewing/ui/activities/MainActivity.kt
@@ -1,6 +1,6 @@
 package com.mibaldi.brewing.ui.activities
 
-import android.support.v7.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.mibaldi.brewing.R
 
