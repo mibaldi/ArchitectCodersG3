b3ad2a44ae41535c275fe09adff42859dcd497cf
Mikel Balduciel Diaz <mibaldi2@gmail.com>
2019-05-07 19:39:59 +0200

    beers repository and firestoredb

--- app/src/main/java/com/mibaldi/brewing/ui/main/MainActivity.kt
+++ app/src/main/java/com/mibaldi/brewing/ui/main/MainActivity.kt
@@ -6,8 +6,9 @@ import com.mibaldi.brewing.R
 import com.mibaldi.brewing.base.activities.BaseActivity
 import com.mibaldi.brewing.interactors.BeerInteractor
 import com.mibaldi.brewing.repositories.FirestoreDB
-import com.mibaldi.brewing.ui.adapters.PubsAdapter
+import com.mibaldi.brewing.ui.adapters.BeersAdapter
 import com.mibaldi.brewing.ui.detail.DetailActivity
+import com.mibaldi.brewing.utils.loadJSONFromAsset
 import com.mibaldi.brewing.utils.observe
 import com.mibaldi.brewing.utils.startActivity
 import com.mibaldi.brewing.utils.withViewModel
@@ -20,28 +21,19 @@ import kotlinx.coroutines.withContext
 class MainActivity : BaseActivity() {
 
     private lateinit var viewModel: MainViewModel
-    private lateinit var adapter: PubsAdapter
+    private lateinit var adapter: BeersAdapter
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        initMock()
         val beerInteractor = BeerInteractor(this)
         viewModel = withViewModel({MainViewModel(beerInteractor)}){
             observe(model,::updateUI)
         }
-        adapter = PubsAdapter(viewModel::onPubClicked)
+        adapter = BeersAdapter(viewModel::onPubClicked)
         recycler.adapter = adapter
     }
 
-    fun initMock(){
-        val context =  this
-        GlobalScope.launch {
-            FirestoreDB.json = withContext(Dispatchers.IO) {FirestoreDB.loadJSONFromAsset(context)}
-            FirestoreDB.addBars()
-        }
 
-
-    }
 
     private fun updateUI(model: MainViewModel.UiModel) {
         progress.visibility = if (model is MainViewModel.UiModel.Loading) View.VISIBLE else View.GONE
@@ -49,7 +41,7 @@ class MainActivity : BaseActivity() {
         when (model) {
             is MainViewModel.UiModel.Content -> adapter.beers = model.beers
             is MainViewModel.UiModel.Navigation -> startActivity<DetailActivity> {
-                putExtra(DetailActivity.PUB, model.beer)
+                putExtra(DetailActivity.BEER, model.beer)
             }
         }
 
