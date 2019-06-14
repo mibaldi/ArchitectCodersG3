d8cb4428aa73b13716e6dd404c0d11a9be217645
Mikel Balduciel Diaz <mibaldi2@gmail.com>
2019-06-14 17:54:24 +0200

    Done profile and emailAndPassword Activities

--- presentation/src/main/java/com/mibaldi/presentation/ui/profile/ProfileActivity.kt
+++ presentation/src/main/java/com/mibaldi/presentation/ui/profile/ProfileActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
 import android.view.View
+import androidx.lifecycle.Observer
 import androidx.databinding.DataBindingUtil
 import com.google.android.material.snackbar.Snackbar
 import com.mibaldi.data.repository.LoginRepositoryImpl
@@ -18,44 +19,39 @@ import com.mibaldi.presentation.databinding.ActivityProfileBinding
 import com.mibaldi.presentation.framework.datasources.LoginDataSourceImpl
 import com.mibaldi.presentation.ui.common.Navigator
 import com.mibaldi.presentation.ui.login.EmailPasswordActivity
+import com.mibaldi.presentation.ui.main.MainViewModel
 import com.mibaldi.presentation.utils.loadUrl
 import com.mibaldi.presentation.utils.observe
 import com.mibaldi.presentation.utils.startActivity
 import com.mibaldi.presentation.utils.withViewModel
 import kotlinx.android.synthetic.main.activity_main.progress
 import kotlinx.android.synthetic.main.activity_profile.*
+import org.koin.android.scope.currentScope
+import org.koin.android.viewmodel.ext.android.viewModel
 
 class ProfileActivity : BaseActivity() {
-    private lateinit var viewModel: ProfileViewModel
-    private lateinit var getCurrentUserInteractor: GetCurrentUserInteractor
-    private lateinit var signOutInteractor: SignOutInteractor
-    private lateinit var removeAccountInteractor: RemoveAccountInteractor
+
+    private val viewModel: ProfileViewModel by currentScope.viewModel(this)
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val binding: ActivityProfileBinding =
-            DataBindingUtil.setContentView(this, R.layout.activity_profile)
-
-        val loginRepository = LoginRepositoryImpl(LoginDataSourceImpl)
-        getCurrentUserInteractor = GetCurrentUserInteractor(loginRepository)
-        signOutInteractor = SignOutInteractor(loginRepository)
-        removeAccountInteractor = RemoveAccountInteractor(loginRepository)
-        viewModel =
-            withViewModel({ ProfileViewModel(Navigator(this),getCurrentUserInteractor, signOutInteractor, removeAccountInteractor) }) {
-                // observe(model, ::updateUI)
-                observe(error,::showError)
-            }
+        setContentView(R.layout.activity_profile)
+        profileToolbar.title = ""
+        setSupportActionBar(profileToolbar)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        viewModel.model.observe(this, Observer(::updateUI))
+        viewModel.model.observe(error,::showError)
         binding.model = viewModel
         binding.lifecycleOwner = this
+        btnLogout.setOnClickListener {
+            viewModel.logout()
+        }
+
         profileToolbar.title = ""
         setSupportActionBar(profileToolbar)
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
 
-
-
-        btnLogout.setOnClickListener {
-            viewModel.logout()
-        }
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
