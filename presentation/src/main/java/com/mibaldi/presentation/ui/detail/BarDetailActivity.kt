7045467b09fb55bf9e9239b2a9c77c9cbc2e3154
Juan Rodríguez <juanagustin.rodriguez@innocv.com>
2019-08-16 11:52:27 +0200

    add bar detail injection

--- presentation/src/main/java/com/mibaldi/presentation/ui/detail/BarDetailActivity.kt
+++ presentation/src/main/java/com/mibaldi/presentation/ui/detail/BarDetailActivity.kt
@@ -1,25 +1,25 @@
 package com.mibaldi.presentation.ui.detail
 
 import android.os.Bundle
-import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import com.mibaldi.presentation.R
+import com.mibaldi.presentation.base.activities.BaseActivity
+import com.mibaldi.presentation.data.model.BarView
 import com.mibaldi.presentation.databinding.ActivityDetailBinding
-import com.mibaldi.presentation.utils.getViewModel
 import com.mibaldi.presentation.utils.snack
 import kotlinx.android.synthetic.main.activity_detail.*
+import org.koin.android.scope.currentScope
+import org.koin.android.viewmodel.ext.android.viewModel
 
-class BarDetailActivity : AppCompatActivity() {
+class BarDetailActivity : BaseActivity() {
     companion object {
         const val BEER = "BarDetailActivity:beer"
     }
 
-    private lateinit var viewModel: BarDetailViewModel
+    private val viewModel: BarDetailViewModel by currentScope.viewModel(this)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-        viewModel = getViewModel { BarDetailViewModel(intent.getParcelableExtra(BEER)) }
 
         val binding: ActivityDetailBinding =
             DataBindingUtil.setContentView(this, R.layout.activity_detail)
@@ -28,7 +28,7 @@ class BarDetailActivity : AppCompatActivity() {
         binding.lifecycleOwner = this
         setSupportActionBar(barDetailToolbar)
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
-
+        viewModel.setData(intent?.extras?.get(BEER) as BarView)
         fab.setOnClickListener { it.snack("Not implemented yet") }
     }
 }
