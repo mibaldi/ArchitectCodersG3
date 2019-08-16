6e5aa2bba3fa468afeffb19aa163f7137d474606
Juan Rodríguez <juanagustin.rodriguez@innocv.com>
2019-08-16 11:52:27 +0200

    add bar detail injection

--- presentation/src/main/java/com/mibaldi/presentation/ui/detail/BarDetailViewModel.kt
+++ presentation/src/main/java/com/mibaldi/presentation/ui/detail/BarDetailViewModel.kt
@@ -3,15 +3,17 @@ package com.mibaldi.presentation.ui.detail
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import com.mibaldi.presentation.base.viewmodel.BaseViewModel
 import com.mibaldi.presentation.data.model.BarView
 
-class BarDetailViewModel(private val barView: BarView) : BaseViewModel() {
+class BarDetailViewModel : ViewModel() {
 
     private val _data = MutableLiveData<BarView>()
     val data: LiveData<BarView>
         get() {
-            if (_data.value == null) _data.value = barView
             return _data
         }
+
+    fun setData(barView: BarView?) {
+        _data.value = barView
+    }
 }
