c04d190ea0033e4cf29f701ba17422dcd8d82a83
Juan Rodríguez <juanagustin.rodriguez@innocv.com>
2019-09-12 20:16:02 +0200

    change entities

--- domain/src/main/java/com/mibaldi/domain/entity/Beer.kt
+++ domain/src/main/java/com/mibaldi/domain/entity/Beer.kt
@@ -2,5 +2,8 @@ package com.mibaldi.domain.entity
 
 data class Beer(
     val id: String,
-    val title: String
+    val title: String,
+    val image: String,
+    val observation: String?,
+    val rating: Float = 0f
 )
