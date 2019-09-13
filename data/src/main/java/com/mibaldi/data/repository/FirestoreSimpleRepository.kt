0156b7381080b3be10022d1415c98a7395429ebe
Juan Rodríguez <juanagustin.rodriguez@innocv.com>
2019-09-13 02:07:07 +0200

    add new interactor

--- data/src/main/java/com/mibaldi/data/repository/FirestoreSimpleRepository.kt
+++ data/src/main/java/com/mibaldi/data/repository/FirestoreSimpleRepository.kt
@@ -1,13 +1,19 @@
 package com.mibaldi.data.repository
 
+import arrow.core.Either
 import com.mibaldi.data.datasource.FirestoreDataSource
 import com.mibaldi.domain.entity.Bar
+import com.mibaldi.domain.exception.RepositoryException
 import com.mibaldi.domain.repository.FirestoreRepository
 
 
 class FirestoreSimpleRepository(
     private val apiSource: FirestoreDataSource
 ) : FirestoreRepository {
+    override suspend fun getBarById(id: String): Either<RepositoryException, Bar> {
+        return apiSource.getBarById(id)
+    }
+
     override suspend fun updateBar(bar: Bar) {
         apiSource.updateBar(bar)
     }
