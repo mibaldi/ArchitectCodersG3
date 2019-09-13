ace80ad502f1c45481f17ac6d351c6e16d0ef829
Juan Rodríguez <juanagustin.rodriguez@innocv.com>
2019-09-13 02:07:07 +0200

    add new interactor

--- domain/src/main/java/com/mibaldi/domain/repository/FirestoreRepository.kt
+++ domain/src/main/java/com/mibaldi/domain/repository/FirestoreRepository.kt
@@ -1,9 +1,12 @@
 package com.mibaldi.domain.repository
 
+import arrow.core.Either
 import com.mibaldi.domain.entity.Bar
+import com.mibaldi.domain.exception.RepositoryException
 
 
 interface FirestoreRepository : BaseRepository {
     suspend fun updateBar(bar: Bar)
     suspend fun getBarsOnce(): List<Bar>
+    suspend fun getBarById(id: String): Either<RepositoryException, Bar>
 }
